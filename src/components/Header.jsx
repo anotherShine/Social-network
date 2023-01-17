a65954b03ad6c417c092ec5a445b558b220418d2
Vova <90705859+anotherShine@users.noreply.github.com>
2023-01-17 21:44:42 +0100

    components  commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className="header">
+    <img src="https://brandmark.io/logo-rank/random/bp.png" alt='bp-logo' />
+  </header>
+}
+export default Header;
