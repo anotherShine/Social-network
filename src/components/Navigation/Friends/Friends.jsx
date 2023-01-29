47ee73f4fca82c24377f091e35f650d1b4a5b1b5
Vova <90705859+anotherShine@users.noreply.github.com>
2023-01-29 01:58:57 +0100

    added nav friends in navigation

---
+++ src/components/Navigation/Friends/Friends.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import classes from './Navigation.module.css';
+
+
+
+const Navigation = () => {
+  return (
+    <div>
+      fdfdf
+    </div>
+)}
+
+export default Navigation;
