bf256ab00d876e748b331466ace78e2f95167de9
Vova <90705859+anotherShine@users.noreply.github.com>
2023-02-10 18:23:33 +0100

    used method bind in index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import './index.css';
-import state from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { addPost, updateNewPostText, subscribe } from './redux/state';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let renderEntireTree = () => {
+let renderEntireTree = (state) => {
   root.render(
     <React.StrictMode>
-      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+      <App state={state} addPost={store.addPost} updateNewPostText={store.updateNewPostText}/>
     </React.StrictMode>
   );
 }
 
-renderEntireTree(state);
-subscribe(renderEntireTree);
+renderEntireTree(store.getState());
+store.subscribe(renderEntireTree);
