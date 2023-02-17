ae5527849ee039a3eb84a8c02ae521db85fffda7
Vova <90705859+anotherShine@users.noreply.github.com>
2023-02-17 15:12:58 +0100

    imported
    imported profileReducer, dialogsReducer, sidebarReducer to redux-store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./index.css";
-import store from "./redux/store";
+import store from "./redux/redux-store";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
