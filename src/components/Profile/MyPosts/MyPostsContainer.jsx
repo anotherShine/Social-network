d53625bd2818be58503ea688b8e58683f880a407
Vova <90705859+anotherShine@users.noreply.github.com>
2023-02-21 23:01:27 +0100

    Update MyPostsContainer.jsx
    created MapStateToProps and mapDispatchToProps for connect MyPostsContainer

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,35 +1,23 @@
 import React from 'react';
 import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/profile-reducer';
-import StoreContext from '../../../StoreContext';
 import MyPosts from './MyPosts';
+import { connect } from 'react-redux';
 
 
 
-
-const MyPostsContainer = (props) => {
-
-  return (
-    <StoreContext.Consumer> 
-      { (store) => {
-        let state = store.getState();
-
-        let addPost = () => {
-          store.dispatch(addPostActionCreator());
-        }
-
-        let onPostChange = (text) => {
-          store.dispatch(updateNewPostTextActionCreator(text));
-        }
-
-        return <MyPosts
-          updateNewPostText={onPostChange}
-          addPost={addPost}
-          posts={state.profilePage.postsData}
-          newPostText={state.profilePage.newPostText} />
-      }
-    }
-    </StoreContext.Consumer>
-  )
+let MapStateToProps = (state) => {
+  return {
+    posts: state.profilePage.postsData,
+    newPostText: state.profilePage.newPostText
+  }
 }
+let mapDispatchToProps = (dispatch) => {
+  return {
+    addPost: () => { dispatch(addPostActionCreator()) },
+    updateNewPostText: (text) => { dispatch(updateNewPostTextActionCreator(text)) }
+  }
+}
+
+const MyPostsContainer = connect(MapStateToProps, mapDispatchToProps)(MyPosts);
 
 export default MyPostsContainer;
