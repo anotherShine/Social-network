67bf5764fc7d8e4598f5be7b7f0e2fb83df46e81
Vova <90705859+anotherShine@users.noreply.github.com>
2023-02-26 18:46:54 +0100

    created
    created users.module.css, added photoUrl in users-reducer.js and changed values in Users in Users.jsx file

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import styles from "./users.module.css"
 
 let Users = (props) => {
     return <div>
-        USERS WILL BE HERE
+        {
+            props.users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photoUrl} alt="man" className={styles.userPhoto}/>
+                    </div>
+                    <div>
+                        <button>Follow</button>
+                    </div>
+                </span>
+                <span>
+                    <span>
+                        <div>{u.fullName}</div><div>{u.status}</div>
+                    </span>
+                    <span>
+                        <div>{u.location.country}</div><div>{u.location.city}</div>
+                    </span>
+                </span>
+            </div>)
+        }
     </div>
 }
 export default Users;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,27 +4,30 @@ const SETUSERS = "SET_USERS"
 
 let initialState = {
   users: [
-    // {
-    //   id: 1,
-    //   followed: false,
-    //   fullName: "Gregor S.",
-    //   status: "student",
-    //   location: { city: "Poznan", country: "Poland" },
-    // },
-    // {
-    //   id: 2,
-    //   followed: true,
-    //   fullName: "Vlodi K.",
-    //   status: "junior frontend dev",
-    //   location: { city: "Kyiv", country: "Ukraine" },
-    // },
-    // {
-    //   id: 3,
-    //   followed: false,
-    //   fullName: "Tom K.",
-    //   status: "senior frontend dev",
-    //   location: { city: "Liverpool", country: "Great Britain" },
-    // },
+    {
+      id: 1,
+      photoUrl: "https://wac-cdn.atlassian.com/ru/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=812",
+      followed: false,
+      fullName: "Gregor S.",
+      status: "student",
+      location: { city: "Poznan", country: "Poland" },
+    },
+    {
+      id: 2,
+      photoUrl: "https://wac-cdn.atlassian.com/ru/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=812",
+      followed: true,
+      fullName: "Vlodi K.",
+      status: "junior frontend dev",
+      location: { city: "Kyiv", country: "Ukraine" },
+    },
+    {
+      id: 3,
+      photoUrl: "https://wac-cdn.atlassian.com/ru/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=812",
+      followed: false,
+      fullName: "Tom K.",
+      status: "senior frontend dev",
+      location: { city: "Liverpool", country: "Great Britain" },
+    },
   ],
 };
 
