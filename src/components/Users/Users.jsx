a005683c035f05a27e11565069d042f0b795c262
Vova <90705859+anotherShine@users.noreply.github.com>
2023-03-03 17:11:47 +0100

    Update Users.jsx
    added function getUsers, what call on Get Users</button>

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,15 +4,18 @@ import axios from "axios";
 import userPhoto from "../../assets-img/images/User-Avatar-Profile-Transparent-Isolated-PNG.png"
 
 let Users = (props) => {
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
     
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-
-            props.setUsers(response.data.items)
-
-        })
+                props.setUsers(response.data.items)
+    
+            })
+        }
     }
+    
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
