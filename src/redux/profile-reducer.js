c2b0ccac14863daee23c414c5920b70fb87043f5
Vova <90705859+anotherShine@users.noreply.github.com>
2023-03-10 20:36:18 +0100

    Update profile-reducer.js
    created case SET_USER_PROFILE and exported it

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,5 +1,6 @@
 const ADD_POST = "ADD_POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE_NEW_POST_TEXT";
+const SET_USER_PROFILE = "SET_USER_PROFILE";
 
 // let initialState делаем переменную по умолчанию для profileReducer, 
 // на случай когда state е будет передан
@@ -9,6 +10,7 @@ let initialState = {
     { id: 1, message: "Hi, how are you?", likesCount: "likes 15" },
     { id: 2, message: "It's my first post", likesCount: "likes 23" },
   ],
+  profile: null
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -31,21 +33,26 @@ const profileReducer = (state = initialState, action) => {
         ...state,
         newPostText: action.newText
       }
+      case SET_USER_PROFILE:
+        return {
+          ...state,
+          profile: action.profile
+        }
     default:
       return state;
   }
 };
 
-export const addPostActionCreator = () => {
-  return {
-    type: "ADD_POST",
-  };
-};
+export const addPostActionCreator = () => ({ type: "ADD_POST"});
+
 export const updateNewPostTextActionCreator = (text) => {
   return {
     type: "UPDATE_NEW_POST_TEXT",
     newText: text,
   };
 };
+
+export const setUserProfile = (profile) => ({ type: "SET_USER_PROFILE", profile});
+
 
 export default profileReducer;
