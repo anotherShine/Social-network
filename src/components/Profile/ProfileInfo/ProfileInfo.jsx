0b176c4b1e8b724bdab3ea0aeb393c6d44cb00e7
Vova <90705859+anotherShine@users.noreply.github.com>
2023-03-10 22:42:47 +0100

    added
    added css class to img

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -12,7 +12,7 @@ const ProfileInfo = (props) => {
         <img src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300" alt="man-with-camera" />
       </div>
       <div className={classes.descriptionBlock}>
-        <img src={props.profile.photos.small} />
+        <img src={props.profile.photos.small} className={classes.userPhoto} />
       </div>
       <div>
         <div>{props.lookingForAJob? `lookingForAJob: ${props.lookingForAJob}`: null}</div>
