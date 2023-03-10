501bcad5f061e2f96712f45047db796fde352312
Vova <90705859+anotherShine@users.noreply.github.com>
2023-03-10 22:18:29 +0100

    Update ProfileInfo.jsx
    created div with user info

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -15,9 +15,18 @@ const ProfileInfo = (props) => {
         <img src={props.profile.photos.small} />
       </div>
       <div>
-        <div>{props.name}</div>
-        <div>{props.uniqueUrlName}</div>
-        <div>{props.status}</div>
+        <div>{props.lookingForAJob? `lookingForAJob: ${props.lookingForAJob}`: null}</div>
+        <div>{props.lookingForAJobDescription? `lookingForAJobDescription: ${props.lookingForAJobDescription}`: null}</div>
+        <div>{props.fullName? `fullName: ${props.fullName}`: null}</div>
+        <div>{props.contacts? `contacts: ${props.contacts}`: null}</div>
+        <div>{props.github? `github: ${props.github}`: null}</div>
+        <div>{props.vk? `vk: ${props.vk}`: null}</div>
+        <div>{props.facebook? `facebook: ${props.facebook}`: null}</div>
+        <div>{props.instagram? `instagram: ${props.instagram}`: null}</div>
+        <div>{props.twitter? `twitter: ${props.twitter}`: null}</div>
+        <div>{props.website? `website: ${props.website}`: null}</div>
+        <div>{props.youtube? `youtube: ${props.youtube}`: null}</div>
+        <div>{props.mainLink? `mainLink: ${props.mainLink}`: null}</div>
       </div>
 
     </div>
