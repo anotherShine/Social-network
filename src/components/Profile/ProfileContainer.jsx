0d0100be0e4e391abfc3525996d32464c4fad9ed
Vova <90705859+anotherShine@users.noreply.github.com>
2023-03-10 20:00:01 +0100

    Update ProfileContainer.jsx
    created props.setUserProfile

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -6,9 +6,7 @@ class ProfileContainer extends React.Component {
   componentDidMount() {
     axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
     .then(response => {
-        this.props.setUsers(response.data.items);
-        this.props.setTotalUsersCount(response.data.totalCount);
-        this.props.toggleIsFetching(false)
+        this.props.setUserProfile(response.data);
     })
   }
   render() {
