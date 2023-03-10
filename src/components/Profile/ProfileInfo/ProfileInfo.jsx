f0560f820d53291e245b74f0fe6a5b5d38df907b
Vova <90705859+anotherShine@users.noreply.github.com>
2023-03-10 20:51:30 +0100

    Update ProfileInfo.jsx
    created img src={props.profile.photos.large}

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -8,6 +8,7 @@ const ProfileInfo = (props) => {
         <img src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300" alt="man-with-camera" />
       </div>
       <div className={classes.descriptionBlock}>
+        <img src={props.profile.photos.large}/>
         ava + description
       </div>
 
